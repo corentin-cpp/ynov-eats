dc24a71ea77e0b744df7a2c633683a4a4ec5b443
root <root@Chouaib>
2025-03-20 14:21:52 +0100

    new color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,7 +52,7 @@ export default function Navbar() {
             ) : (
               <Link
                to="/login"
-                className="bg-red-600 text-white px-4 py-2 rounded-full hover:bg-red-700 transition"
+                className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-700 transition"
               >
                 Connexion
               </Link>
